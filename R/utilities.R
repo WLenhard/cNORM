838d15696b90b301c4e30686a9c8cef33c09ae58
Alexandra Lenhard <lenhard@psychometrica.de>
2018-09-08 17:55:58 +0200

    old functions deleted

--- R/utilities.R
+++ R/utilities.R
@@ -1,86 +1,3 @@
-#' Internal method for creating a skewed normal distribution
-#'
-#' The function generates a skewed normal distribution based on the SN
-#' package by Adelchi Azzalini (http://azzalini.stat.unipd.it/SN/). It
-#' (is used for internal testing purposes only)
-#' @param n number of observations
-#' @param location mean of the distribution
-#' @param scale the standard deviation
-#' @param slant Measure for skewness
-#' @return a skewed normal distribution
-#' @references Azzalini, A. with the collaboration of Capitanio, A. (2014).
-#' The Skew-Normal and Related Families. Cambridge University Press,
-#' IMS Monographs series.
-skewedNormal <- function(n, location, scale, slant = 10) {
-  u1 <- stats::rnorm(n)
-  u2 <- stats::rnorm(n)
-  id <- (u2 > slant * u1)
-  u1[id] <- (-u1[id])
-  return(as.vector(location + scale * u1))
-}
-
-#' Simulate curve linear and skewed sample data
-#'
-#' @param n number of observations
-#' @param minAge the lower age bound
-#' @param maxAge the upper age bound
-#' @param minRaw the lower bound of raw scores
-#' @param maxRaw the upper bound of raw scores
-#' @param nGroups number of samples, that should be drawn
-#' @param pow Power factor. Values < 1 represent root functions (e. g. pow = .5)
-#' @param slant Measure for skewness within each sample. Use 0 for symetric normal distributions,
-#' values > 1 for positively skewed data (e. g. 10), values lower than 0 for negatively skewed
-#' distributions. The according function is based on the SN package by Azzalini (2014)
-#' @param k The power paramerer for the computePowers and bestMode function
-#' @return The simulated sample data set with ranking and powers already applied
-#' @references Azzalini, A. with the collaboration of Capitanio, A. (2014). The Skew-Normal and Related Families. Cambridge University Press, IMS Monographs series.
-#' @export
-#' @examples
-#' sample <- simData(100, minAge = 1, maxAge = 4, nGroups = 4)
-#' model <- bestModel(sample)
-#' plotPercentiles(sample, model)
-simData <- function(n, minAge = 1, maxAge = 4, minRaw = 0, maxRaw = 50, nGroups = 4, pow = 1, slant = 0, k = 4) {
-
-  # first generate linear distribution of groups, mean and sd
-  groups <- seq(minAge, maxAge, length = nGroups)
-
-  min <- (maxRaw - minRaw) / (nGroups + 1)
-  max <- (maxRaw - minRaw) * (nGroups / (nGroups + 1))
-  m <- seq(min, max, length = nGroups)
-  sd <- rep(min / 2, nGroups)
-
-  fac <- (m / max)^pow
-  m <- fac * max
-  sd <- sd * fac
-
-  i <- 1
-  sample <- data.frame(matrix(ncol = 2, nrow = 0))
-  names(sample) <- c("group", "raw")
-
-  # generate sample data, so far drawn from normal distribution
-  while (i <= nGroups) {
-    f <- data.frame(matrix(ncol = 2, nrow = n))
-    names(f) <- c("group", "raw")
-
-    f$group <- rep(groups[[i]], n)
-    f$raw <- skewedNormal(n, m[[i]], sd[[i]], slant = slant)
-    sample <- rbind(sample, f)
-    i <- i + 1
-  }
-
-  # restrict value range
-  sample$raw[sample$raw < minRaw] <- minRaw
-  sample$raw[sample$raw > maxRaw] <- maxRaw
-
-  # ranking and powers
-  sample <- rankByGroup(sample)
-  sample <- computePowers(sample, k)
-
-
-  return(sample)
-}
-
-
 #' Simulate mean per age
 #'
 #' @param age the age variable
@@ -92,7 +9,7 @@ simData <- function(n, minAge = 1, maxAge = 4, minRaw = 0, maxRaw = 50, nGroups 
 #' x <- simMean(a)
 #' }
 simMean <- function(age){
-  return(age - 0.05 * age^2 + 0.0001 * age^4)
+  return(1.5*age - 0.05 * age^2 + 0.0001 * age^4)
 }
 
 #' Simulate sd per age
@@ -106,7 +23,7 @@ simMean <- function(age){
 #' x <- simSD(a)
 #' }
 simSD <- function(age){
-  return(((age * 0.25 + 1)^0.3)*0.5)
+  return((1.5*age - 0.05 * age^2 + 0.0001 * age^4)*0.2+1)
 }
 
 #' Simulate raw test scores based on Rasch model
@@ -138,6 +55,24 @@ simSD <- function(age){
 #' @export
 #'
 #' @examples
+#' # simulate data
+#' sim <- simulateRasch(n=150, minAge=1, maxAge=7, items.n = 30, items.m = -1.0, items.sd = 1, randomTheta = TRUE, width = 1.0)
+#'
+#' # Show item difficulties
+#' View(sim$theta)
+#' mean(sim$theta)
+#' sd(sim$theta)
+#' describe(sim$theta)
+#' hist(sim$theta)boxplot(raw~group, data=sim$data)
+#'
+#' # Plot raw scores
+#' boxplot(raw~group, data=sim$data)
+#'
+#' # Model data
+#' data <- prepareData(sim$data, age="age")
+#' model <- bestModel(data, k = 4)
+#' printSubset(model)
+#' plotSubset(model, type=0)
 simulateRasch <- function(n = 100, minAge = 1, maxAge=7, items.n = 21, items.m = 0, items.sd = 1, randomTheta = TRUE, width = 1){
   # draw sample
   groups <- seq.int(from = minAge, to = maxAge)
